b73f6a78d0976c1806a027becbaedef54980ede6
tekdoken <tekdokenzx@gmail.com>
2021-09-30 15:17:01 +0700

    case

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 window.onload=anMenu()
 let a=true
 function anMenu() {
-    document.getElementById("noimenu").style.display = "none";
+    document.getElementById("left").style.display = "none";
 }
 function hienMenu() {
-    document.getElementById("noimenu").style.display = "flex";
+    document.getElementById("left").style.display = "inline-block";
     a=false
 }
 function menu(){
@@ -14,4 +14,42 @@ function menu(){
         anMenu();
         a=true
     }
+}
+
+
+window.onload=andk()
+let d=true
+function andk() {
+    document.getElementById("fdk").style.display = "none";
+}
+function hiendk() {
+    document.getElementById("fdk").style.display = "inline-block";
+    d=false
+}
+function dk(){
+    if(n==true){
+    if(d==true){
+        hiendk()
+    }else{
+        andk();
+        d=true
+    }}
+}
+window.onload=andn()
+let n=true
+function andn() {
+    document.getElementById("fdn").style.display = "none";
+}
+function hiendn() {
+    document.getElementById("fdn").style.display = "inline-block";
+    n=false
+}
+function dn(){
+    if(d==true){
+    if(n==true){
+        hiendn()
+    }else{
+        andn();
+        n=true
+    }}
 }
